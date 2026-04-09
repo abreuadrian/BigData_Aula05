14eabb24a11a7ab0b59cddc733c1288dd8416a07
abreu.adrian <abreuadrianoliv@gmail.com>
2026-04-09 20:01:12 -0300

    initial commit

---
+++ aula05/exemplo05.py
@@ -0,0 +1,24 @@
+#Ex01.: match case
+n = int(input("""-----Menu-----
+                 
+[1] - Marketing\n[2] - Financeiro\n[3,4,5] - ADM\n[6,7,8,9] - T.I.\n[10] - Serviços Gerais
+
+Informe um número de acesso: """))
+
+match n:
+    case 1:
+        print('Marketing')
+    case 2:
+        print('Financeiro')
+    case 3|4|5:
+        print('ADM')
+    case 6|7|8|9:
+        print('T.I.')
+    case 10:
+        print('Serviços Gerais')
+    case _:
+        print('Código não encontrado')
+
+#Ex02.:
+
+
