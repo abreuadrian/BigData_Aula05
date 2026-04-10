7a6d798e7eefc4b6d45b99562019f105262fe355
abreu.adrian <abreuadrianoliv@gmail.com>
2026-04-09 21:48:41 -0300

    add ex03

--- aula05/atividade05_pt2.py
+++ aula05/atividade05_pt2.py
@@ -3,6 +3,6 @@ n = int(input('Informe um número inteiro: '))
 
 match n:
     case i if 0 > n:
-        print(f'{n} é um número negativo')
+        print(f'[{n}] é um número negativo')
     case i if 0 <= n:
-        print(f'{n}é um número positivo')
+        print(f'[{n}] é um número positivo')
--- aula05/exemplo05.py
+++ aula05/exemplo05.py
@@ -33,3 +33,65 @@ match age:
         print('Idoso')
     case _:
         print('Idade inválida')
+
+#Ex03.: forma de pagamento:
+price = float(input('Informe o valor da compra: '))
+payment = int(input("""-----Menu-----
+                    
+[1] - PIX (12% de desconto)
+[2] - DÉBITO (8% de desconto)
+[3] - CRÉDITO (5% de juros)
+[4] - DINHEIRO (15% de desconto)
+                    
+Escolha a forma de pagamento: """))
+
+match payment:
+    case 1:
+        print('\nVocê escolheu a opção PIX\n')
+        print(f'Sua compra de R${price:.2f}, ganhou o desconto de R${price * 0.12:.2f}\nO valor final ficou em R${price - (price * 0.12):.2f}')
+    case 2:
+        print('\nVocê escolheu a opção DÉBITO\n')
+        print(f'Sua compra de R${price:.2f}, ganhou o desconto de R${price * 0.08:.2f}\nO valor final ficou em R${price - (price * 0.08):.2f}')
+    case 3:
+        print('\nVocê escolheu a opção CRÉDITO\n')
+        print(f'Sua compra de R${price:.2f} teve um JUROS de R${price * 0.05:.2f}\nO valor final ficou em R${price + (price * 0.05):.2f}')
+    case 4:
+        print('\nVocê escolheu a opção DINHEIRO\n')
+        print(f'Sua compra de R${price:.2f}, ganhou o desconto de R${price * 0.15:.2f}\nO valor final ficou em R${price - (price * 0.15):.2f}')
+    case _:
+        print('Erro: Opção inválida')
+
+#---- OUTRA FORMA DE RESOLUÇÃO -----
+desc = 0
+price = float(input('Informe o valor da compra: '))
+payment = int(input("""-----Menu-----
+                    
+[1] - PIX (12% de desconto)
+[2] - DÉBITO (8% de desconto)
+[3] - CRÉDITO (5% de juros)
+[4] - DINHEIRO (15% de desconto)
+                    
+Escolha a forma de pagamento: """))
+
+match payment:
+    case 1:
+        print('\nVocê escolheu a opção PIX\n')
+        desc = price * 0.12
+    case 2:
+        print('\nVocê escolheu a opção DÉBITO\n')
+        desc = price * 0.08
+    case 3:
+        print('\nVocê escolheu a opção CRÉDITO\n')
+        desc = price * 0.05
+    case 4:
+        print('\nVocê escolheu a opção DINHEIRO\n')
+        desc = price * 0.15
+    case _:
+        print('Erro: Opção inválida')
+
+if desc != 0:
+    new_price = price - desc
+    print(f'Valor: R${price:.2f}')
+    print(f'Valor do desconto: R${desc:.2f}')
+    print(f'Valor final: R${new_price:.2f}')
+else: print('Informe uma das opções acima')
